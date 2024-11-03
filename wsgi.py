2962f1bcc578a0bb82d51f7a7db8bdef4bbacfd3
abhi2005lakhera <138852367+abhi2005lakhera@users.noreply.github.com>
2024-11-04 00:53:22 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
-from app import app
+from flask import Flask
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
