7358debd4cbb1e8829b5da04f3310e4745316652
abhi2005lakhera <138852367+abhi2005lakhera@users.noreply.github.com>
2024-11-04 00:08:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, send_file, request, jsonify
+import os
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+@app.route('/')
+def home():
+    return send_file('./templates/index.html')
+
+@app.route('/style.css')
+def serve_css():
+    return send_file('style.css')
+
+UPLOAD_FOLDER = 'pics'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'message': 'No file part in the request'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+    if file:
+        # Save the file in the 'pics' folder
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filepath)
+        return jsonify({'message': 'File uploaded successfully'}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
